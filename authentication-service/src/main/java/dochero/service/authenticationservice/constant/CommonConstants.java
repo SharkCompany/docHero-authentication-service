b26ecc05b60637552a93d61a02d0a13e59c0ef69
phongvanngo <19520834@gm.uit.edu.vn>
2023-01-02 00:41:54 +0700

    fix constant expired time

--- authentication-service/src/main/java/dochero/service/authenticationservice/constant/CommonConstants.java
+++ authentication-service/src/main/java/dochero/service/authenticationservice/constant/CommonConstants.java
@@ -1,5 +1,5 @@
 package dochero.service.authenticationservice.constant;
 
 public interface CommonConstants {
-   long EXPIRE_DURATION = ; // 1 month
+   long EXPIRE_DURATION =  30L * 24 * 60 * 60 * 1000; ; // 1 month
 }
